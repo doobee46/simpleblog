f6249b94dd0ba9eecb8f429f303818fe0bca1739
obedlorisson <obed.lorisson@gmail.com>
2013-05-14 01:42:06 -0400

    initial commit

---
+++ app/models/comment_observer.rb
@@ -0,0 +1,7 @@
+class CommentObserver < ActiveRecord::Observer
+
+  def email_article_author
+  	puts "we will notify the author in chapter 9"
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'b004b7c544a00881168d54df141d5aeeaeaf4f163f0ca68431dc42afab502773893f2eaafddabd5c6ef55f926e36248dc59e58d16a5449d70ca72ae9c571388e'
